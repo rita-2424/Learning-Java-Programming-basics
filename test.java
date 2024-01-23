aa15590da6ed931d5fcc18b754c5da1586d9caf8
Rita Njoki O <118954238+rita-2424@users.noreply.github.com>
2024-01-23 11:23:23 +0300

    Update

---
+++ test.java
@@ -0,0 +1,5 @@
+public class Welcome{
+    public static void main(String[] args){
+        System.out.println("Welcome to Java Programming." + "\n" + "A Pure object oriented programming language");
+    }
+}
---
+++ welcome.java
@@ -0,0 +1,6 @@
+public class Welcome{
+    Run | Debug
+    public static void main(String[] args){
+        System.out.printIn("Welcome to Java Programming."\n" A Pure object oriented pragramming language");
+}
+}
